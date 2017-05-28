d4a0f78fdb8bd59dacdf45db341916a5342dec12
guitarbum722 <johnkenneth.moore@gmail.com>
2017-05-28 15:50:44 -0700

    prepare uri and call call() for getting a group by group_id

--- groups.go
+++ groups.go
@@ -1,12 +1,28 @@
 package meetup
 
 import (
+	"fmt"
 	"github.com/briandowns/meetup-client/models"
+	"net/url"
 )
 
+const groupsEndpoint = "/2/groups"
+
 // GroupByID returns the data for a single meetup group using the specified groupID
-func (c *Client) GroupByID(groupID int) (*models.Group, error) {
-	return nil, nil
+func (c *Client) GroupByID(groupID int) (*models.Groups, error) {
+	var groups models.Groups
+
+	v := url.Values{}
+	v.Set("group_id", strconv.Itoa(groupID))
+	v.Set("key", c.opts.APIKey)
+
+	uri := fmt.Sprintf("%s?%s", groupsEndpoint, v.Encode())
+
+	if err := c.call(http.MethodGet, uri, nil, &groups); err != nil {
+		return nil, err
+	}
+
+	return &groups, nil
 }
 
 // GroupByURLName returns the data for a single meetup group using the specified urlName
