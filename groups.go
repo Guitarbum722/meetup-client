87ccf48085b20a725487ae6af964547d82d85e0c
guitarbum722 <johnkenneth.moore@gmail.com>
2017-05-28 11:35:42 -0700

    get a single group by its url name or its group id

---
+++ groups.go
@@ -0,0 +1,15 @@
+package meetup
+
+import (
+	"github.com/briandowns/meetup-client/models"
+)
+
+// GroupByID returns the data for a single meetup group using the specified groupID
+func (c *Client) GroupByID(groupID int) (*models.Group, error) {
+	return nil, nil
+}
+
+// GroupByURLName returns the data for a single meetup group using the specified urlName
+func (c *Client) GroupByURLName(urlName string) (*models.Group, error) {
+	return nil, nil
+}
