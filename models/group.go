5b1e47e8e7e7a9262e97f44ae078e16ef91528e1
guitarbum722 <johnkenneth.moore@gmail.com>
2017-05-28 11:35:04 -0700

    add group and groups models which will likely be reused throughout the rest of the lib

---
+++ models/group.go
@@ -0,0 +1,18 @@
+package models
+
+// Group describes a meetup group
+type Group struct {
+	Name        string `json:"name"`
+	URLName     string `json:"urlname"`
+	ID          int    `json:"id"`
+	Link        string `json:"link"`
+	MemberCount int    `json:"members"`
+}
+
+// Groups wraps a slice of Group for unmarshalling the results array.
+// It also contains meta fields from the response.
+type Groups struct {
+	Groups     []Group `json:"results"`
+	TotalCount int     `json:"total_count"`
+	Count      int     `json:"count"`
+}
