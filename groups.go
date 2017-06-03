8f71b8ef2777b9ee737b28bcff88f11b5905d606
guitarbum722 <johnkenneth.moore@gmail.com>
2017-06-02 20:42:45 -0700

    add methods to query a group by urlName, groupID or organizerID

--- groups.go
+++ groups.go
@@ -2,11 +2,15 @@ package meetup
 
 import (
 	"fmt"
-	"github.com/briandowns/meetup-client/models"
+	"github.com/Guitarbum722/meetup-client/models"
+	"net/http"
 	"net/url"
+	"strconv"
 )
 
-const groupsEndpoint = "/2/groups"
+const (
+	groupsEndpoint = "/2/groups"
+)
 
 // GroupByID returns the data for a single meetup group using the specified groupID
 func (c *Client) GroupByID(groupID int) (*models.Groups, error) {
@@ -14,7 +18,7 @@ func (c *Client) GroupByID(groupID int) (*models.Groups, error) {
 
 	v := url.Values{}
 	v.Set("group_id", strconv.Itoa(groupID))
-	v.Set("key", c.opts.APIKey)
+	v.Add("key", c.opts.APIKey)
 
 	uri := fmt.Sprintf("%s?%s", groupsEndpoint, v.Encode())
 
@@ -26,11 +30,35 @@ func (c *Client) GroupByID(groupID int) (*models.Groups, error) {
 }
 
 // GroupByURLName returns the data for a single meetup group using the specified urlName
-func (c *Client) GroupByURLName(urlName string) (*models.Group, error) {
-	return nil, nil
+func (c *Client) GroupByURLName(urlName string) (*models.Groups, error) {
+	var groups models.Groups
+
+	v := url.Values{}
+	v.Set("group_urlname", urlName)
+	v.Add("key", c.opts.APIKey)
+
+	uri := groupsEndpoint + queryStart + v.Encode()
+
+	if err := c.call(http.MethodGet, uri, nil, &groups); err != nil {
+		return nil, err
+	}
+
+	return &groups, nil
 }
 
 // GroupByOrganizer returns the data for a single meetup group using the specified organizerID
-func (c *Client) GroupByOrganizer(organizerID int) (*models.Group, error) {
-	return nil, nil
+func (c *Client) GroupByOrganizer(organizerID int) (*models.Groups, error) {
+	var groups models.Groups
+
+	v := url.Values{}
+	v.Set("organizer_id", strconv.Itoa(organizerID))
+	v.Add("key", c.opts.APIKey)
+
+	uri := groupsEndpoint + queryStart + v.Encode()
+
+	if err := c.call(http.MethodGet, uri, nil, &groups); err != nil {
+		return nil, err
+	}
+
+	return &groups, nil
 }
