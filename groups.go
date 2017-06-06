33c55a12bf2913e41de745780c895ecd917d9155
guitarbum722 <johnkenneth.moore@gmail.com>
2017-06-05 20:30:03 -0700

    fix existing comments and add method to get group data by zip code

--- groups.go
+++ groups.go
@@ -11,7 +11,7 @@ const (
 	groupsEndpoint = "/2/groups"
 )
 
-// GroupByID returns the data for a single meetup group using the specified groupID
+// GroupByID returns the meetup groups using the specified groupIDs
 // The response contains an array of results, even if there is only one because the request can
 // consist of comma separated values as the group_id parameter.
 func (c *Client) GroupByID(groupIDs []int) (*models.Groups, error) {
@@ -34,7 +34,7 @@ func (c *Client) GroupByID(groupIDs []int) (*models.Groups, error) {
 	return &groups, nil
 }
 
-// GroupByURLName returns the data for a single meetup group using the specified urlName
+// GroupByURLName returns the group data using the specified urlNames
 // The response contains an array of results, even if there is only one because the request can
 // consist of comma separated values as the group_id parameter.
 func (c *Client) GroupByURLName(urlNames []string) (*models.Groups, error) {
@@ -52,7 +52,7 @@ func (c *Client) GroupByURLName(urlNames []string) (*models.Groups, error) {
 	return &groups, nil
 }
 
-// GroupByOrganizer returns the data for a single meetup group using the specified organizerID
+// GroupByOrganizer returns the data the specified meetup groups using organizerIDs
 // The response contains an array of results, even if there is only one because the request can
 // consist of comma separated values as the group_id parameter.
 func (c *Client) GroupByOrganizer(organizerIDs []int) (*models.Groups, error) {
@@ -64,6 +64,22 @@ func (c *Client) GroupByOrganizer(organizerIDs []int) (*models.Groups, error) {
 
 	v := c.urlValues()
 	v.Add("organizer_id", strings.Join(convIDs, ","))
+
+	uri := groupsEndpoint + queryStart + v.Encode()
+
+	var groups models.Groups
+	if err := c.call(http.MethodGet, uri, nil, &groups); err != nil {
+		return nil, err
+	}
+
+	return &groups, nil
+}
+
+// GroupByZip returns the group data for the specified zip code
+func (c *Client) GroupByZip(zipCode int) (*models.Groups, error) {
+
+	v := c.urlValues()
+	v.Add("zip", strconv.Itoa(zipCode))
 
 	uri := groupsEndpoint + queryStart + v.Encode()
 
