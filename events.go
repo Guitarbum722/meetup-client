1c374fb04d29063abfe4b4e58ca62054fbea68ed
guitarbum722 <johnkenneth.moore@gmail.com>
2017-06-09 21:08:13 -0700

    add Event and applicable structs

--- events.go
+++ events.go
@@ -1,1 +1,34 @@
 package meetup
+
+import (
+	"net/http"
+
+	"github.com/Guitarbum722/meetup-client/models"
+)
+
+const (
+	smartRadius        = "smart"
+	findEventsEndpoint = "/find/events"
+)
+
+// EventsByGeo returns event data based on latitude, longitude and radius respectively.
+// Radius can be a value of 'smart', 'global', or in between 0 and 100
+// If an empty string is passed for radius, then 'smart' will be used as a default
+func (c *Client) EventsByGeo(lat, lon, radius string) ([]models.Event, error) {
+	v := c.urlValues()
+	if radius == "" {
+		radius = smartRadius
+	}
+	v.Add("radius", radius)
+	v.Add("lon", lon)
+	v.Add("lat", lat)
+
+	uri := findEventsEndpoint + queryStart + v.Encode()
+
+	var events []models.Event
+	if err := c.call(http.MethodGet, uri, nil, &events); err != nil {
+		return nil, err
+	}
+
+	return events, nil
+}
--- models/event.go
+++ models/event.go
@@ -1,1 +1,22 @@
 package models
+
+// Event describes a meetup event and pertinent information such as id, comments, etc.
+type Event struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Link     string `json:"link"`
+	YesRSVP  int    `json:"yes_rsvp_count"`
+	Waitlist int    `json:"waitlist_count"`
+	Group    `json:"group"`
+	Venue    `json:"venue"`
+}
+
+// Comment is a meetup event comment
+type Comment struct {
+}
+
+// Venue ...
+type Venue struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
